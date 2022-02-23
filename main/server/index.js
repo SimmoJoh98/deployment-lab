660f8fb9a4787e4823da9aca0cefb2431ca527fc
Xeilon <simmojoh98@gmail.com>
2022-02-23 13:22:16 -0700

    5th attempt

--- main/server/index.js
+++ main/server/index.js
@@ -4,11 +4,11 @@ const path = require('path')
 const app = express()
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../index.html')).status(200);
 });
+app.get('/favicon.ico', (req,res) => res.sendStatus(200))
 
 const port = process.env.PORT || 4005
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
